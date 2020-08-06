901968e0e6d4a53282bc94c50167a7c41aeaa47a
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-08-06 00:07:41 -0700

    Animated social media icons, other style changes.

--- src/components/MyFooter.tsx
+++ src/components/MyFooter.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import SocialMediaIcons from "../components/SocialMediaIcons";
-import "../styles/SocialMediaIcons.css";
 
 function MyFooter() {
   return (
     <div>
       <h1>MyFooter!</h1>
-      <SocialMediaIcons />
     </div>
   );
 }
