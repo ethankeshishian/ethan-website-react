28c331a6b11d8424d86016e7271707a06a54b6b7
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-08-06 10:43:10 -0700

    Minor style updates.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -10,7 +10,7 @@ function Body() {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       />
-      <div>
+      <div className="main-container">
         <div className="content-container">
           <div className="image-container">
             <img className="image" src={Ethan} alt="Ethan Keshishian" />
