956f58b4a44d1616a38feaca5b5cd744218391cb
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-08-06 23:38:30 -0700

    Updated email address with ethan@ethank.tech.

--- src/constants.tsx
+++ src/constants.tsx
@@ -5,4 +5,4 @@ export const GITHUB_LINK = "https://github.com/ethankeshishian";
 export const INSTAGRAM_LINK = "https://twitter.com/ethankeshishian";
 export const TWITTER_LINK = "https://instagram.com/ethankeshishian";
 export const SPOTIFY_LINK = "https://open.spotify.com/user/baklou";
-export const EMAIL_LINK = "mailto:keshishianethan+site@gmail.com";
+export const EMAIL_LINK = "mailto:ethan@ethank.tech";
