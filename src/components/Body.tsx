f5a807d506910c22b3cfd52cb6863ecef5543cbe
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-08-08 00:05:56 -0700

    Flex centered body, flex centered text.
    TODO: Media Queries

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -16,14 +16,16 @@ function Body() {
             <img className="image" src={Ethan} alt="Ethan Keshishian" />
           </div>
           <div className="text-container">
-            <h2 className="title">Hi, I'm Ethan</h2>
-            <p className="bio">
-              I’m a computer science student at UCLA, class of 2023. Check out
-              my LinkedIn for more on what I do, or check out my GitHub for
-              information on some of my projects.
-            </p>
-            <div className="social-icons">
-              <SocialMediaIcons />
+            <div className="text-content-container">
+              <h2 className="title">Hi, I'm Ethan</h2>
+              <p className="bio">
+                I’m a computer science student at UCLA, class of 2023. Check out
+                my LinkedIn for more on what I do, or check out my GitHub for
+                information on some of my projects.
+              </p>
+              <div className="social-icons">
+                <SocialMediaIcons />
+              </div>
             </div>
           </div>
         </div>
