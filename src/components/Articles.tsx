88d5f4772af0c700c827e5ec2e80cd4f2e7f0940
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-08-30 19:31:25 -0700

    Cleanup.

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -5,11 +5,9 @@ import "../styles/Articles.css";
 function Articles() {
   return (
     <div>
-      <h2 className="articles-header">Articles:</h2>
+      <h2 className="articles-header">Articles (Coming Soon):</h2>
       <div className="articles-container">
-        <Article url="https://jsonplaceholder.typicode.com/posts/1" />
-        <Article url="https://jsonplaceholder.typicode.com/posts/1" />
-        <Article url="https://jsonplaceholder.typicode.com/posts/1" />
+        {/* <Article url="https://jsonplaceholder.typicode.com/posts/1" /> */}
       </div>
     </div>
   );
