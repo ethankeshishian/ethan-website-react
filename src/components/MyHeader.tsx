d89e4921e3710e4db2953d55962ad339f5e538c0
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-05-11 15:06:14 -0700

    Updated heading

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -4,8 +4,8 @@ import '../styles/MyHeader.css';
 function MyHeader() {
   return (
     <div className="heading-container">
-      <a href="#" className="main-heading">
-        <h1>Ethan Keshishian</h1>
+      <a href="/" className="main-heading-link">
+        <h1 className="main-heading">Ethan Keshishian</h1>
       </a>
       <p className="subtext">
         Software Engineer | Founder | UCLA Computer Science
