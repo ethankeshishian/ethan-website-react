db9a2dad8f01ff127dcd256363b8334ce214e39b
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-07-17 14:45:18 -0700

    Added dark mode button

--- src/redux/configureStore.tsx
+++ src/redux/configureStore.tsx
@@ -1,8 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import { rootReducer } from './reducers';
 
+/* Used to make Redux extension compatible with TypeScript */
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => {
-  let store = createStore(rootReducer);
+  let store = createStore(rootReducer, composeEnhancers());
   return { store };
 };
