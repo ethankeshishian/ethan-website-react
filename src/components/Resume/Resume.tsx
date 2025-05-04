32ece4cd91bf942570d291f19dbdeb80168e958e
Ethan Keshishian <keshishianethan@gmail.com>
2025-05-03 20:22:34 -0700

    Making dark resume glassy, background gradient animation, fixing project card font size

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -11,47 +11,49 @@ function Resume() {
 
   return (
     <div className="resume-main-block">
-      <div className="resume-cards-container">
-        <h4 className="resume-section-title">Education</h4>
-        {educationData.map((item) => (
-          <div className="resume-card">
-            <div className="resume-card-image-container">
-              <img className="resume-card-image" src={require("../../assets/logos/" + item.logo).default} alt={item.logo} />
-            </div>
-            <div>
-              <h5 className="resume-card-header">{item.accreditation.inputStr}</h5>
-              <div className="resume-subtitle">
-                {item.organization}
-                {" • "} {item.dates.rawText}
+      <span className="resume-container-wrapper">
+        <Squircle className="resume-cards-container" cornerRadius={8} borderWidth={1}>
+          <h4 className="resume-section-title">Education</h4>
+          {educationData.map((item) => (
+            <div className="resume-card">
+              <div className="resume-card-image-container">
+                <img className="resume-card-image" src={require("../../assets/logos/" + item.logo).default} alt={item.logo} />
               </div>
               <div>
-                {item.description.split("\n").map((line) => (
-                  <div className="resume-line">{line}</div>
-                ))}
+                <h5 className="resume-card-header">{item.accreditation.inputStr}</h5>
+                <div className="resume-subtitle">
+                  {item.organization}
+                  {" • "} {item.dates.rawText}
+                </div>
+                <div>
+                  {item.description.split("\n").map((line) => (
+                    <div className="resume-line">{line}</div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-        <h4 className="resume-section-title">Experience</h4>
-        {workData.map((item) => (
-          <div className="resume-card">
-            <div className="resume-card-image-container">
-              <img className="resume-card-image" src={require("../../assets/logos/" + item.logo).default} alt={item.logo} />
-            </div>
-            <div>
-              <h5 className="resume-card-header">{item.jobTitle}</h5>
-              <div className="resume-subtitle">
-                {item.organization} {" • "} {item.dates.rawText}
+          ))}
+          <h4 className="resume-section-title">Experience</h4>
+          {workData.map((item) => (
+            <div className="resume-card">
+              <div className="resume-card-image-container">
+                <img className="resume-card-image" src={require("../../assets/logos/" + item.logo).default} alt={item.logo} />
               </div>
               <div>
-                {item.jobDescription.split("\n").map((line) => (
-                  <div className="resume-line">{line}</div>
-                ))}
+                <h5 className="resume-card-header">{item.jobTitle}</h5>
+                <div className="resume-subtitle">
+                  {item.organization} {" • "} {item.dates.rawText}
+                </div>
+                <div>
+                  {item.jobDescription.split("\n").map((line) => (
+                    <div className="resume-line">{line}</div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </Squircle>
+      </span>
       <br />
       <h4 className="project-section-title">Projects</h4>
       <div className="project-cards-container">
